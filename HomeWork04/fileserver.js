b77e07d6459497abe536cf4bae5b5402a8a75e55
yared Girma <yared.dabi@gmail.com>
2017-07-01 14:45:11 -0500

    changed

--- HomeWork04/fileserver.js
+++ HomeWork04/fileserver.js
@@ -3,31 +3,22 @@ var fs = require('fs');
 var url = require('url');
 var querystring = require('querystring');
 var request = require('request');
-var path = require('path');
 
 http.createServer(function(req,res){
-      //  console.log('request recieved..');
         var urlStr = req.url;
         var query = url.parse(urlStr).query;
         var path = querystring.parse(query)['url'];
-    //     var readStream = fs.createReadStream(__dirname+'/'+path,{encoding:'utf8'});
-    //    // readStream.pipe(res);
-       // readStream.on('error',function(err){
-         //   res.end('Invalid File Path ... File Not found');
-       // });
-
-var rf = fs.readFile(path.join(__dirname+'/'+path,{encoding:'utf8'}));
-    res.end(rf);
-
-
+        var readStream = fs.createReadStream(__dirname+'/'+path,{encoding:'utf8'});
+        readStream.pipe(res);
+        readStream.on('error',function(err){
+            res.end('Invalid Path');
+        });
 
 }).listen(4000);
 
-console.log('Listening on port 4000');
-
 request('http://localhost:4000/?url=path/to/my/file.txt',function(err,res,body){
     if (!err && res.statusCode == 200) {
-        console.log(body) // Print the body of response.
+        console.log(body) 
     }
     else{
         console.log(err);
